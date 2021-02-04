eef4ec918a44991a1af70762807ac64f45f4a99d
Ariel Gordon <arielgordon123@gmail.com>
2021-02-04 11:16:20 +0200

    change manifest

--- devtools.js
+++ devtools.js
@@ -4,3 +4,27 @@ x.onload = function() {
     chrome.devtools.panels.applyStyleSheet(x.responseText);
 };
 x.send();
+
+
+
+var page_getProperties = function() {
+    var data = window.$IND && $0 ? $IND.data($0) : {};
+    // Make a shallow copy with a null prototype, so that sidebar does not
+    // expose prototype.
+    var props = Object.getOwnPropertyNames(data);
+    var copy = { __proto__: null };
+    for (var i = 0; i < props.length; ++i)
+        copy[props[i]] = data[props[i]];
+    return copy;
+}
+
+chrome.devtools.panels.elements.createSidebarPane(
+    "jQuery Properties",
+    function(sidebar) {
+        function updateElementProperties() {
+            sidebar.setExpression("(" + page_getProperties.toString() + ")()");
+        }
+        updateElementProperties();
+        chrome.devtools.panels.elements.onSelectionChanged.addListener(
+            updateElementProperties);
+    });
