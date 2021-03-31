e5e837f2583721e08fc80394c82cbfb340c6280b
Ariel Gordon <arielgordon123@gmail.com>
2021-03-31 12:04:52 +0300

    Update devtools.js
    Add React, jQuery, $IND

--- devtools.js
+++ devtools.js
@@ -19,10 +19,61 @@ var page_getProperties = function() {
 }
 
 chrome.devtools.panels.elements.createSidebarPane(
-    "jQuery Properties",
+    "$IND",
     function(sidebar) {
         function updateElementProperties() {
             sidebar.setExpression("(" + page_getProperties.toString() + ")()");
+        }
+        updateElementProperties();
+        chrome.devtools.panels.elements.onSelectionChanged.addListener(
+            updateElementProperties);
+    });
+
+var page_getProperties2 = function() {
+    var data = window.jQuery && $0 ? jQuery.data($0) : {};
+    // Make a shallow copy with a null prototype, so that sidebar does not
+    // expose prototype.
+    var props = Object.getOwnPropertyNames(data);
+    var copy = { __proto__: null };
+    for (var i = 0; i < props.length; ++i)
+        copy[props[i]] = data[props[i]];
+    return copy;
+}
+
+chrome.devtools.panels.elements.createSidebarPane(
+    "jQuery",
+    function(sidebar) {
+        function updateElementProperties() {
+            sidebar.setExpression("(" + page_getProperties2.toString() + ")()");
+        }
+        updateElementProperties();
+        chrome.devtools.panels.elements.onSelectionChanged.addListener(
+            updateElementProperties);
+    });
+
+
+const findReactComponent = () => {
+    let el = $0;
+    const data = {}
+    for (const key in el) {
+        if (key.startsWith('__reactInternalInstance$') || key.startsWith('__reactFiber$')) {
+            const fiberNode = el[key];
+            data.Instance = { fiberNode, 'fiberNode.return': {...fiberNode.return }, 'stateNode': {...fiberNode.return.stateNode } };
+        }
+        if (key.startsWith('__reactEventHandlers$') || key.startsWith('__reactProps$')) {
+            const fiberNode = el[key];
+            data.Handlers = {...fiberNode };
+        }
+    }
+
+    return data ? data : null;
+};
+
+chrome.devtools.panels.elements.createSidebarPane(
+    "React",
+    function(sidebar) {
+        function updateElementProperties() {
+            sidebar.setExpression("(" + findReactComponent.toString() + ")()");
         }
         updateElementProperties();
         chrome.devtools.panels.elements.onSelectionChanged.addListener(
